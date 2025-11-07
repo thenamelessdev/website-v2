5b814b906966cc4602fb28590ab889e7f74f2e22
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-11-07 19:56:17 +0000

    made main file

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+//imports
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+import http from "http";
+
+//vars and conf
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+app.use(express.json());
+app.set("view engine", "ejs");
+const port = process.env.port || 8080;
+
+//routes
+
+//main
+app.get("/", (req: Request, res: Response) => {
+    res.render("index");
+});
+
+server.listen({port, host: "0.0.0.0"});
