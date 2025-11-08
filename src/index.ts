c2543074684a49fbea168b78e50d068a90b7e953
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-11-08 08:20:29 +0000

    added styles and who am i page

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,23 @@ app.use(express.json());
 app.set("view engine", "ejs");
 const port = process.env.port || 8080;
 
+//rootdir
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootdir = path.join(__dirname, "..");
+
 //routes
 import projectsRouter from "./routes/projects.js";
 app.use("/projects", projectsRouter);
 //main things
 app.get("/", (req: Request, res: Response) => {
     res.render("index");
+});
+app.get("/who", (req: Request, res: Response) => {
+    res.render("who");
+});
+app.get("/style.css", (req: Request, res: Response) => {
+    res.sendFile(rootdir + "/views/style.css");
 });
 
 server.listen({port, host: "0.0.0.0"});
