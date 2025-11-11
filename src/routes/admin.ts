c1cf64854699cbef85db4c29f4267bb1958bd8c3
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-11-11 18:31:09 +0000

    added cloudflare turnstile for admin pages

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,22 @@
-import express, { Request, Response } from "express";
+import { error } from "console";
+import express, { Request, Response, urlencoded } from "express";
 const router = express.Router();
+const secret = process.env.CfSecret || "im missing";
+
+async function verify(token: string) {
+    const verify = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({ secret, response: token }),
+    });
+    const result = await verify.json();
+    if(result.success) {
+        return true;
+    }
+    else{
+        return false
+    }
+}
 
 router.get("/", (req: Request, res: Response) => {
     if (req.session.adminUname && req.session.adminUname == process.env.adminUname){
@@ -10,12 +27,18 @@ router.get("/", (req: Request, res: Response) => {
     }
 });
 
-router.post("/", (req: Request, res: Response) => {
-    const { username, password } = req.body;
+router.post("/", async (req: Request, res: Response) => {
+    const { username, password, } = req.body;
+    const token = req.body["cf-turnstile-response"];
 
     if(username == process.env.adminUname && password == process.env.adminPassw){
-        req.session.adminUname = username;
-        res.render("admin/panel");
+        if(await verify(token)) {
+            req.session.adminUname = username;
+            res.render("admin/panel");
+        }
+        else{
+            res.render("error", { error: "Cloudflare turnstile failed. Please try again." });
+        }
     }
     else {
         res.render("error", { error: "Wrong admin username or password" });
