69c7bf6b0a49420043e2eb6819e07f848cba40f0
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2026-01-06 20:38:37 +0100

    added cf turnstile

--- src/routes/typeword.ts
+++ src/routes/typeword.ts
@@ -1,15 +1,30 @@
 import express, { Request, Response } from "express";
+import { verify } from "../functions.js";
 const router = express.Router();
+
+interface typeWordRoom {
+    name: string,
+    host: string,
+    player?: string,
+    winner?: string,
+}
+
+let rooms: Record<string, typeWordRoom> = {};
 
 router.get("/", (req: Request, res: Response) => {
     res.render("projects/typeword/index")
 });
 
-router.post("/create", (req: Request, res: Response) => {
+router.post("/create", async (req: Request, res: Response) => {
     const { username, room } = req.body;
 
     if(username && room){
-        
+        if(await verify(req)){
+
+        }
+        else{
+            res.render("error", { error: "Cloudflare turnstile failed" });
+        }
     }
     else{
         res.status(400).render("error", { error: "missing username or room name" });
